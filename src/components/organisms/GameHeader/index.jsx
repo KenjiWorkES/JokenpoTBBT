975818e01cc95bf23fd7902b685bf1f0552f2ed0
KenjiLucas <802.182@alunos.unigran.br>
2022-06-23 14:34:57 -0400

    adcionando espaçamentos e efeito de hover nos coins

--- src/components/organisms/GameHeader/index.jsx
+++ src/components/organisms/GameHeader/index.jsx
@@ -2,7 +2,7 @@ import { GameScore } from "@/components/molecules";
 
 const GameHeader = () => {
   return (
-    <section className="header">
+    <section className="header u-margin-bottom-medium">
       <img className="header__logo" src="/assets/logo-bonus.svg" />
       <GameScore />
     </section>
